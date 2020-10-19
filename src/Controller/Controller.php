e5e0b3cdaea7c4f06144631b6309f9c2a5210d6e
MERLEY Léa <lea.merley@orange.fr>
2020-10-19 16:22:36 +0200

    commit

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -21,9 +21,7 @@ class Controller
 
         $projet = $projectRepository->find(1);
         $employee = $employeeRepository->find(214);
-
         $projects = $projectRepository->findByEmployee($employee);
-
         dd($employee);
         include '../templates/index.php';
         ob_end_flush();
