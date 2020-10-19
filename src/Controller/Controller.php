3210dcdb4693eaa6c732ffc6a7f78fc4561e4b19
sicnarf92 <fr.gueret@live.fr>
2020-10-19 09:20:39 +0200

    drei

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -1,5 +1,31 @@
 <?php
 class Controller
 {
-    
+    public function homeController()
+    {
+        ob_start();
+        include '../templates/index.php';
+        ob_end_flush();
+    }
+
+    public function loginController()
+    {
+        ob_start();
+        include '../templates/login.php';
+        ob_end_flush();
+    }
+
+    public function signupController()
+    {
+        ob_start();
+        include '../templates/signup.php';
+        ob_end_flush();
+    }
+
+    public function error404Controller()
+    {
+        ob_start();
+        include '../templates/404.php';
+        ob_end_flush();
+    }
 }
