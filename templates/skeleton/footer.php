76e17983d49bf305cfca6b925c21c078d574b250
MERLEY Léa <lea.merley@orange.fr>
2020-10-19 13:00:30 +0200

    commited

--- templates/skeleton/footer.php
+++ templates/skeleton/footer.php
@@ -1,1 +1,39 @@
-il est ou le cucul?
+<div class="container">
+    <footer class="row justify-content-md-center mt-4">
+        <div class="col-md-3 bg-dark mx-auto text-white">
+            <h4 class="text-center">Suivez-nous !</h4>
+                <a href="https://www.facebook.com/" target="_blank">
+                    <img src="assets/image/logo_fb.png" alt="Logo FB">
+                </a>
+                <a href="https://twitter.com/" target="_blank">
+                    <img src="assets/image/logo_twitter.jpg" alt="Logo Twitter">
+                </a>
+                <a href="https://www.instagram.com/" target="_blank">
+                    <img src="assets/image/logo_instagram.png" alt="Logo Instagram">
+                </a>   
+        </div>
+        <div class="col-md-3 bg-dark mx-auto text-white">
+            <h4 class="text-center">Informations Générales</h4>
+            <ul>
+                <li>Conditions</li>
+                <li>Politique de confidentialité</li>
+                <li>Cookies</li>
+                @2020 Abi, Inc
+            </ul>
+        </div>
+        <div class="col-md-3 bg-dark mx-auto text-white">
+            <h4 class="text-center">Nous Contacter</h4>
+            Webmaster :<br/>
+            <a href="mailto:abifictive@gmail.com">abifictive@gmail.com</a>
+        </div>
+    </footer>
+</div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!--<script src="assets/js/jquery-3.5.1.slim.min.js"></script>-->
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/database.js"></script>
+    <script src="assets/js/connexion.js" ></script>
+</body>
+</html>
