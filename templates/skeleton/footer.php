49723efa68ed9e6dfe36f0bd50a724baef9ce6be
sicnarf92 <fr.gueret@live.fr>
2020-10-22 11:51:16 +0200

    Update footer.php

--- templates/skeleton/footer.php
+++ templates/skeleton/footer.php
@@ -1,5 +1,5 @@
     <footer class="row rounded-top mx-1">
-        <div class="col-md-4 bg-dark rounded-top mx-auto text-white">
+        <div class="col-md-4 rounded-top mx-auto ">
             <h4 class="text-center">Suivez-nous !</h4>
                 <a href="https://www.facebook.com/" target="_blank">
                     <img src="assets/image/logo_fb.png" alt="Logo FB">
@@ -11,7 +11,7 @@
                     <img src="assets/image/logo_instagram.png" alt="Logo Instagram">
                 </a>   
         </div>
-        <div class="col-md-4 bg-dark mx-auto text-white">
+        <div class="col-md-4  mx-auto ">
             <h4 class="text-center">Informations Générales</h4>
             <ul>
                 <li>Conditions</li>
@@ -20,7 +20,7 @@
                 @2020 Abi, Inc
             </ul>
         </div>
-        <div class="col-md-4 bg-dark rounded-top mx-auto text-white">
+        <div class="col-md-4  rounded-top mx-auto ">
             <h4 class="text-center">Nous Contacter</h4>
             Webmaster :<br/>
             <a href="mailto:abifictive@gmail.com">abifictive@gmail.com</a>
